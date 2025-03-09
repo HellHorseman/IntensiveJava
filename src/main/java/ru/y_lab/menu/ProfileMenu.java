228cff7f2cbefe618325c163d7de259b350ae7cc
HellHorseman <duke.sheffer@yandex.ru>
2025-03-09 21:31:19 +0400

    fix profile

--- src/main/java/ru/y_lab/menu/ProfileMenu.java
+++ src/main/java/ru/y_lab/menu/ProfileMenu.java
@@ -11,20 +11,22 @@ public class ProfileMenu {
     static void showProfileMenu(Scanner scanner) {
         while (true) {
             System.out.println("\n--- Профиль пользователя ---");
-            System.out.println("1. Обновить профиль");
-            System.out.println("2. Удалить аккаунт");
-            System.out.println("3. Назад");
+            System.out.println("1. Показать профиль");
+            System.out.println("2. Обновить профиль");
+            System.out.println("3. Удалить аккаунт");
+            System.out.println("4. Назад");
             System.out.print("Выберите действие: ");
             int choice = scanner.nextInt();
             scanner.nextLine();
 
             switch (choice) {
-                case 1 -> UserActions.updateProfile(scanner); // Обновляем профиль
-                case 2 -> {
+                case 1 -> UserActions.showProfile();
+                case 2 -> UserActions.updateProfile(scanner); // Обновляем профиль
+                case 3 -> {
                     UserActions.deleteAccount(); // Удаляем аккаунт
                     showMainMenu(scanner); // Возвращаемся в меню после удаления аккаунта
                 }
-                case 3 -> {
+                case 4 -> {
                     return; // Возвращаемся в главное меню
                 }
                 default -> System.out.println("Некорректный ввод.");
